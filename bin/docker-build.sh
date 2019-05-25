652b54cbf6e5e9f3618d3ebae62560a5e569b390
rorono <kosuke19952000@gmail.com>
2019-05-26 00:17:15 +0900

    Update

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -2,5 +2,6 @@
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
 docker build \
+    -t rorono/development:latest \
     -t rorono/development:${COMMIT_HASH} \
     .
--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
+docker push rorono/development:latest 
 docker push rorono/development:${COMMIT_HASH}
