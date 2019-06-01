def6b982399d190667d13833a42684a392ca5abc
rorono <kosuke19952000@gmail.com>
2019-06-01 13:16:45 +0900

    Update

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 COMMIT_HASH=$(git rev-parse --short HEAD)
+
 docker build \
     -t rorono/development:latest \
     -t rorono/development:${COMMIT_HASH} \
-    .
+    ${SCRIPT_DIR}/../src
--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
+
 docker push rorono/development:latest 
 docker push rorono/development:${COMMIT_HASH}
