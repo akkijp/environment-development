ae58631418c55505c75cac6b7d50ebfd032a0839
Kosuke Akizuki <7535743+rorono@users.noreply.github.com>
2019-06-04 20:20:30 +0900

    Update docker-entrypoint.sh

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-eval "$(anyenv init -)"
+
+# anyenv configuration
+if [ -d $HOME/.anyenv ] ; then
+    export PATH="$HOME/.anyenv/bin:$PATH"
+    eval "$(anyenv init -)"
+fi
+
 exec "$@"
